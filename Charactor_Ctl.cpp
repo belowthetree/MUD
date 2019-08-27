94502aa32c10598ea43605ab58db6f4f00fefd3d
belowthetree <37807774+belowthetree@users.noreply.github.com>
2019-08-27 14:37:09 +0800

    完善物品相关

--- Charactor_Ctl.cpp
+++ Charactor_Ctl.cpp
@@ -20,28 +20,114 @@ void Charactor_Ctl::Daily_Recovery()
 }
 
 // 添加物品
-void Charactor_Ctl::AddGoods(Goods good)
+bool Charactor_Ctl::AddGoods(Goods good)
 {
-	goods.push_back(good);
+	if (goods.size() < maxCarried)
+		goods.push_back(good);
+	else
+		return false;
+	return true;
 }
 
 // 显示物品列表
 void Charactor_Ctl::ShowGoods()
 {
 	for (int i = 0; i < goods.size(); i++)
-		cout << i + 1 << "、" << goods[i].name << endl;
+		cout << i + 1 << ". " << goods[i].name << endl;
 }
 
 // 用于删除某个物品，成功则返回true，否则false
-bool Charactor_Ctl::DeleteGoods(string name)
+bool Charactor_Ctl::DeleteGoods(int i)
 {
+	int cnt = 0;
 	for(vector<Goods>::iterator it = goods.begin();it != goods.end();it++)
-		if ((*it).name == name)
+		if (cnt++ == i)
 		{
 			goods.erase(it);
 			return true;
 		}
 	return false;
+}
+
+int Charactor_Ctl::GetSpeed()
+{
+	int sum = this->speed;
+	for (vector<Goods>::iterator it = carried.begin(); it != carried.end(); it++)
+	{
+		sum += (*it).speed;
+	}
+	return sum;
+}
+
+int Charactor_Ctl::GetPower()
+{
+	int sum = this->power;
+	for (vector<Goods>::iterator it = carried.begin(); it != carried.end(); it++)
+	{
+		sum += (*it).power;
+	}
+	return sum;
+}
+
+int Charactor_Ctl::GetDefense()
+{
+	int sum = this->defense;
+	for (vector<Goods>::iterator it = carried.begin(); it != carried.end(); it++)
+	{
+		sum += (*it).defense;
+	}
+	return sum;
+}
+
+bool Charactor_Ctl::AddCarried(Goods good)
+{
+	for (vector<Goods>::iterator it = carried.begin(); it != carried.end(); it++)
+	{
+		if ((*it).type == good.type)
+		{
+			return false;
+		}
+	}
+	carried.push_back(good);
+	return true;
+}
+
+void Charactor_Ctl::DeleteCarried(int id)
+{
+	int cnt = 0;
+	for (vector<Goods>::iterator it = carried.begin(); it != carried.end(); it++)
+	{
+		if (cnt++ == id)
+		{
+			carried.erase(it);
+			return;
+		}
+	}
+}
+
+bool Charactor_Ctl::PutOffCarried(int id)
+{
+	if(goods.size() == maxCarried)
+		return false;
+	
+	int cnt = 0;
+	for (vector<Goods>::iterator it = carried.begin(); it != carried.end(); it++)
+	{
+		if (cnt++ == id)
+		{
+			goods.push_back((*it));
+			carried.erase(it);
+			return true;
+		}
+	}
+}
+
+void Charactor_Ctl::ShowCarried()
+{
+	for (int i = 0; i < carried.size(); i++)
+	{
+		cout << i + 1 << ". " << carried[i].name << endl;
+	}
 }
 
 // 设定初始值
@@ -77,9 +163,12 @@ Charactor_Ctl::Charactor_Ctl(Work work, int xp, int mMagic, int level, int money
 	XP = xp;
 	this->level = level;
 	this->money = money;
+	maxCarried = 10;
 }
 
 
 Charactor_Ctl::~Charactor_Ctl()
 {
+	carried.clear();
+	goods.clear();
 }
--- Goods.cpp
+++ Goods.cpp
@@ -2,9 +2,13 @@
 
 
 
-Goods::Goods(string name)
+Goods::Goods(string name, int power, int defense, int speed, int value)
 {
 	this->name = name;
+	this->power = power;
+	this->defense = defense;
+	this->speed = defense;
+	this->value = value;
 }
 
 
